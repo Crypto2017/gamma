e8577fd989a4544e6b0d179144f67b867838b73f
Crypto2017 <paulcronin2016@gmail.com>
2017-08-28 21:14:13 +0200

    Updated db.py
    -DataBase class
    -Unit class
    -Table class
    -Pool class
    -MultiThreadDB class
    
    --> some functions

--- gamma/db.py
+++ gamma/db.py
@@ -2,10 +2,25 @@ import pickle
 class DataBase():
 	def __init__(self,path="."):
 		self.path=path
+		
+	def createTable(self,name):
+		pass
+	def createPool(self,name):
+		pass
+	def get(self,name):
+		pass
+	def __getitem__(self,name):
+		return self.get(name)
 
 class Unit():
 	def __init__(self,path):
 		self.path=path
+	def get(self,key):
+		pass
+	def set(self,key,inf):
+		pass
+	def update(self,key,inf):
+		pass		
 
 class Table(Unit):
 	def __init__(self,path):
@@ -13,4 +28,9 @@ class Table(Unit):
 		
 class Pool(Unit):
 	def __init__(self,path):
+		pass
+	
+class MultiThreadDB(Unit):
+	def __init__(self,path):
+		self.index={}
 		pass
